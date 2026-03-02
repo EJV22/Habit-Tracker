106cf52f82f400cc10fb72219072316cba7323f1
ej.vite <viteeiron@gmail.com>
2026-03-01 20:41:34 -0500

    Refactor habit tracking functionality
    Refactor functions for sleep and screen time input, add todo list management, and implement data saving/loading.

--- Task and Hour tracker.py
+++ Task and Hour tracker.py
@@ -1,28 +1,3 @@
-'''
-Habit Tracker!
-
-
-    Outlines when you went to bed in the recent week, your weekly water intake,
-what your goals are, and compares it to the previous week's stats.
-
-
-    Do you ever feel disorganized or seem to struggle with creating an effective schedule?
-Maybe you want to be more consistent with your goals and wish to keep track of your progress.
-This app/program helps you plan out your day-to-day life and keeps you in the loop of your own
-goals and progress. It handles things such as bedtime and weekly reminders or
-to-do lists. How does it do that? Well the program asks for what thing you want to keep track of
-then stores the information you give it (ex. what time you went to bed), it can then relay back
-all the information stored whenever you need it! What if you need to set a list of reminders
-throughout the week? Well do not worry, it will also store various tasks that you can access
-through the 'to-do' menu. All the progress is stored with an option, and if you want to
-start over, it allows you to reset weekly.
-
-It uses an Input file called habit_data.txt
-
-'''
-import os
-print(os.getcwd())
-
 #--------------------------Functions---------------------------------
 
 
@@ -337,3 +312,4 @@ ______________________
 
 if __name__ == '__main__':
     main()
+
